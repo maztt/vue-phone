2fe847be3bb685bae922d92f95a63ebde5d84af5
Júlio Mazotti <92126792+maztt@users.noreply.github.com>
2023-07-26 16:21:59 -0300

    feat: add router handler

--- api/src/server.js
+++ api/src/server.js
@@ -1,10 +1,10 @@
 const express = require('express')
+const router = require('./routes/router')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(express.json())
+app.use('/api/v1', router)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
