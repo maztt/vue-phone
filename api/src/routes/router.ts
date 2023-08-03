c4bf3d95356f1294268c46195fc1e3570ea59087
Júlio Mazotti <92126792+maztt@users.noreply.github.com>
2023-08-03 15:29:27 -0300

    feat: add api/v1/add post route

--- api/src/routes/router.ts
+++ api/src/routes/router.ts
@@ -1,10 +1,21 @@
 import express, { Router } from 'express'
 import { Request, Response } from 'express';
+import { AppController } from '../controllers/app.controller';
+import { requestFieldsValidator } from '../helpers/request-fields-validator';
+import { responseToUser } from '../helpers/response-to-user';
 
 const router: Router = express.Router();
 
 router.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
 });
+router.post('/add', async (req: Request, res: Response) => {
+  const isValid = requestFieldsValidator(req.body, res)
+  if (isValid !== true) return
+
+  const added = await AppController.add(req.body)
+  if (!added) return responseToUser(500, 'Failed to add contact', res)
+  return responseToUser(201, `Contact ${added.name} added successfully.`, res)
+})
 
 export default router;
