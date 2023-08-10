a5b1f4a1f6b002cec513c35ad3ada9230c6ced47
Júlio Mazotti <92126792+maztt@users.noreply.github.com>
2023-08-10 15:11:45 -0300

    refactor: enhance AppController.add code legibility

--- api/src/controllers/app.controller.ts
+++ api/src/controllers/app.controller.ts
@@ -1,3 +1,4 @@
+import { ContactsRepository } from '../repositories/contacts-repository';
 import { AddContactDTO } from './dto/add-contact.dto'
 import sqlite3 from 'sqlite3';
 
@@ -6,19 +7,8 @@ const db = new sqlite3.Database(dbPath);
 
 export class AppController {
     static async add (data: AddContactDTO): Promise<AddContactDTO> {
-        const { name, phone, email, picture } = data
-        const query = 'INSERT INTO contacts (name, phone, email, picture) VALUES (?, ?, ?, ?)'
-        const values = [name, phone, email, picture]
-
-        db.run(query, values, (err) => {
-            if (err) {
-                console.error('Error while adding a contact: ', err.message)
-                return false
-            }
-            console.log('Contact added to the database successfully!')
-        })
-
-        return data
+        const added = await ContactsRepository.insert(data)
+        return added
     }
 
     static async list (): Promise<AddContactDTO[]> {
